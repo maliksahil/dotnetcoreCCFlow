20f88b16135750285ba2f7f6985060d3fb8c838c
Tess DiStefano <tess.distefano@gmail.com>
2019-08-20 11:48:22 -0700

    get run local
    code working
    
    remove usings
    
    all but admin consent
    
    make share friendly

--- client/Program.cs
+++ client/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Identity.Client;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Linq;
 using System.Net.Http;
-using System.Security.Cryptography.X509Certificates; // Only import this if you are using certificate
 using System.Threading.Tasks;
+
+// new for this
+using System.Security;
 
 namespace client
 {
@@ -38,38 +38,33 @@ namespace client
         private static async Task RunAsync()
         {
             AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
-            bool isUsingClientSecret = AppUsesClientSecret(config);
-            IConfidentialClientApplication app;
+            IPublicClientApplication app;
 
-            if (isUsingClientSecret)
-            {
-                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                    .WithClientSecret(config.ClientSecret)
-                    .WithAuthority(new Uri(config.Authority))
-                    .WithLogging(Log,
-                        LogLevel.Info,
-                       enablePiiLogging: true,
-                       enableDefaultPlatformLogging: true)
-                    .Build();
-            }
+            app = PublicClientApplicationBuilder.Create(config.ClientId)
+                .WithAuthority(new Uri(config.Authority)) // TODO maybe not needed
+                .WithLogging(Log,
+                    LogLevel.Info,
+                    enablePiiLogging: true,
+                    enableDefaultPlatformLogging: true)
+                .Build();
 
-            else
-            {
-                X509Certificate2 certificate = ReadCertificate(config.CertificateName);
-                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                    .WithCertificate(certificate)
-                    .WithAuthority(new Uri(config.Authority))
-                    .Build();
-            }
 
-            string[] scopes = new string[] { 
+            string[] scopes = new string[] {
                 $"https://{config.Domain}/api/.default"
-                };
+            };
+
+            // Get Token
+            string username = "[USERNAME]";
+            SecureString password = new SecureString();
+            foreach (char a in "[PASSWORD]") {
+                password.AppendChar(a);
+            }
 
             AuthenticationResult result = null;
             try
             {
-                result = await app.AcquireTokenForClient(scopes)
+                // TODO first check if somebody is logged in
+                result = await app.AcquireTokenByUsernamePassword(scopes, username, password)
                     .ExecuteAsync();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Token acquired");
@@ -81,7 +76,14 @@ namespace client
                 Console.WriteLine("Scope provided is not supported");
                 Console.ResetColor();
             }
+            catch (MsalUiRequiredException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("MISC UI Required Exception, see sample to handle more robustly: https://github.com/Azure-Samples/active-directory-dotnetcore-console-up-v2/blob/db1941a634228585f6badc658d8228e0f35dc538/up-console/PublicAppUsingUsernamePassword.cs");
+                Console.ResetColor();
+            }
 
+            // Use Token
             if (result != null)
             {
                 var httpClient = new HttpClient();
@@ -93,47 +95,6 @@ namespace client
         private static void Display(string result)
         {
             Console.WriteLine(result);
-        }
-
-        private static bool AppUsesClientSecret(AuthenticationConfig config)
-        {
-            string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
-            string certificatePlaceholderValue = "[Or instead of client secret: Enter here the name of a certificate (from the user cert store) as registered with your application]";
-
-            if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
-            {
-                return true;
-            }
-
-            else if (!String.IsNullOrWhiteSpace(config.CertificateName) && config.CertificateName != certificatePlaceholderValue)
-            {
-                return false;
-            }
-
-            else
-                throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
-        }
-
-        private static X509Certificate2 ReadCertificate(string certificateName)
-        {
-            if (string.IsNullOrWhiteSpace(certificateName))
-            {
-                throw new ArgumentException("certificateName should not be empty. Please set the CertificateName setting in the appsettings.json", "certificateName");
-            }
-            X509Certificate2 cert = null;
-
-            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
-            {
-                store.Open(OpenFlags.ReadOnly);
-                X509Certificate2Collection certCollection = store.Certificates;
-                // Find unexpired certificates.
-                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
-                // From the collection of unexpired certificates, find the ones with the correct name.
-                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certificateName, false);
-                // Return the first certificate in the collection, has the right name and is current.
-                cert = signingCert.OfType<X509Certificate2>().OrderByDescending(c => c.NotBefore).FirstOrDefault();
-            }
-            return cert;
         }
     }
 }
--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 # az login
 # you'll need to specify your own identifier-uri, and update your appsettings.json in both projects accordingly.
-az ad app create \\
-    --display-name sampleApp \\
-    --identifier-uris https://sahilmalikgmail.onmicrosoft.com/api  \\
-    --oauth2-allow-implicit-flow true  \\
-    --reply-urls "https://localhost:44377/"  \\
-    --password clientsecretpasswordmakethisrandom
+az ad app create --display-name newappname --reply-urls "https://localhost:44377/" --oauth2-allow-implicit-flow true --password e791fcb9-6234-4327-bd01-bd43754ca15f
+
+# consent to app
+# TODO figure out how to get app id in a way that it can be sent as parameter here, for now just run this manually
+#az ad app permission admin-consent --id "[?displayName == 'newappname'].appId"
 
 # Get the client ID 
-az ad app list --query "[?displayName == 'sampleApp'].appId"
+az ad app list --query "[?displayName == 'newappname'].appId"
